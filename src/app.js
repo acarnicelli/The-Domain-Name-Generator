6d852e2c042a889e4cc539a5a726764e76a0af36
acarnicelli <alecarnicelli@hotmail.com>
2021-08-28 00:22:16 +0000

    Ejercicio The Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  // My code
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".net", ".us", ".io"];
+
+  pronoun.forEach(pronoun => {
+    adj.forEach(adj => {
+      noun.forEach(noun => {
+        domain.forEach(domain => {
+          console.log(pronoun + adj + noun + domain);
+        });
+      });
+    });
+  });
+
   console.log("Hello Rigo from the console!");
 };
